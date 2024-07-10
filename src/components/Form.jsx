bac1a52e95bde9cb3e520a85032c65be4a7e1dfe
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-07-10 22:26:28 +0200

    controlled element

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,36 @@
+import { useState } from 'react';
+
 function Form() {
+    const [description, setDescription] = useState('');
+    const [quantity, setQuantity] = useState(1);
+
     function handleSubmit(event) {
         event.preventDefault();
 
-        console.log(event);
+        if (!description) return;
+
+        const newItem = {
+            description,
+            quantity,
+            packed: false,
+            id: Date.now(),
+        };
+
+        console.log(newItem);
+
+        setDescription('');
+        setQuantity(1);
     }
 
     return (
         <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
 
-            <select>
+            <select
+                value={quantity}
+                // не забываем строку преобразовать в число
+                onChange={(e) => setQuantity(Number(e.target.value))}
+            >
                 {/* создаем динамически массив с 20 элементами */}
                 {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
                     <option value={num} key={num}>
@@ -17,7 +38,12 @@ function Form() {
                     </option>
                 ))}
             </select>
-            <input type="text" placeholder="Item..." />
+            <input
+                type="text"
+                placeholder="Item..."
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
             <button>Add</button>
         </form>
     );
